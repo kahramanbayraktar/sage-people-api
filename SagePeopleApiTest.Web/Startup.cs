bd0be3def0602f5437df8c0df356d19d40ef6ca6
Kahraman Bayraktar <kahraman.bayraktar@ite-turkey.com>
2019-04-22 16:14:19 +0300

    Allowed developer exception page on production.

--- SagePeopleApiTest.Web/Startup.cs
+++ SagePeopleApiTest.Web/Startup.cs
@@ -38,15 +38,18 @@ namespace SagePeopleApiTest.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseHsts();
+
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //    app.UseHsts();
+            //}
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
